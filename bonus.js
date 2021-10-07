f2bba12612816669d0d54a86741513f96a16d900
EloiGry <eloi.grychta@gmail.com>
2021-10-07 17:33:34 +0200

    modif

---
+++ bonus.js
@@ -0,0 +1,13 @@
+var moment = require ("moment")
+
+// var jeudi = moment().format('dddd')
+
+// console.log(`Today, we are ${jeudi}`)
+
+
+// var m = moment().format('MMMM')
+// console.log(`We are the month of ${m}`)
+
+// var today = moment().format("DD/MM/YYYY")  
+// console.log(today)
+
---
+++ index.js
@@ -0,0 +1,60 @@
+// function whatDayIsToday () {
+//     var aujo = new Date()
+//     var auj = aujo.getDay()
+//     var weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+//     // console.log(`Today, we are ${weekdays[auj]}`)
+// }
+
+// whatDayIsToday ()
+
+// function whatMonthIsIt () {
+//     var d = new Date()
+//     var m = d.getMonth()
+//     var month = ["January","February","March","April","May","June","July",
+//             "August","September","October","November","December"];
+//             // console.log(`We are the month of ${month[m]}`)
+// }
+
+// whatMonthIsIt ()
+
+// function formatDate () {
+//     var fd = new Date()
+//     var y = fd.getFullYear()
+//     var m = fd.getMonth()
+//     var d = fd.getDay()
+
+//     if (m < 10 && d < 10) {
+//         console.log (`${y}/0${m}/0${d}`)
+//     } else if (m < 10) {
+//         console.log (`${y}/0${m}/${d}`)
+//     } else if (m < 10) {
+//         console.log (`${y}/${m}/0${d}`)
+//     } else {
+//         console.log (`${y}/${m}/${d}`)
+//     }
+// }
+// formatDate ()
+
+// function calculateAge (jour,mois,année) {
+//     var fd = new Date()
+//     var y = fd.getFullYear()
+//     var m = fd.getMonth()
+//     var d = fd.getDay()
+//     var total = y - année 
+    
+
+//     if (mois > m) {
+//         console.log (total -1)
+//     } else if  (mois === m && jour > d) {
+//         console.log (total -1)
+//     } else {
+//         console.log (total)
+//     }
+    
+
+// }
+
+// calculateAge (24,12,1990)
+
+
+
